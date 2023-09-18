3d80cb45dc1c56ea1a7d2019d438a0faf4ddda64
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-09-18 19:22:28 -0400

    Initial commit

---
+++ src/main/java/io/github/thepoultryman/cookingwportals/CookingWithPortals.java
@@ -0,0 +1,10 @@
+package io.github.thepoultryman.cookingwportals;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CookingWithPortals implements ModInitializer {
+    @java.lang.Override
+    public void onInitialize() {
+
+    }
+}
